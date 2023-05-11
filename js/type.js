9bf58df404e4b6b281858e9c396385f44fc6ce4a
Renzyyyy <renzcollincapena187@gmail.com>
2023-05-11 13:38:22 +0800

    update type script

---
+++ js/type.js
@@ -0,0 +1,6 @@
+const typed = new Typed('#element', {
+    strings: ['FRONT END DEVELOPER', 'BACK END DEVELOPER', 'FULL STACK DEVELOPER'],
+    typeSpeed:70,
+    backSpeed:70,
+    loop : true,
+  });
