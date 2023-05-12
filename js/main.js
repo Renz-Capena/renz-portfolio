8c340f8748555d2fd36783a355ac2f523acd20f7
Renzyyyy <renzcollincapena187@gmail.com>
2023-05-12 11:01:32 +0800

    add responsive nav amd hero

--- js/main.js
+++ js/main.js
@@ -27,3 +27,48 @@ themeBtn.addEventListener("click",function(){
 
     }
 })
+
+
+// ================== NAVIGATION FOR RESPONSIVE
+const nav = document.querySelector("nav")
+const navBtn = document.querySelector(".navBtn")
+const closeBtnNav = document.querySelector(".closeBtnNav")
+
+// nav bg layer
+const bgLayer = document.querySelector(".bg_layer_nav")
+
+const navToggle_show = () => {
+    
+    nav.classList.add("nav_show")
+    navBtn.classList.add("display_hide")
+    bgLayer.classList.add("bg_layer_nav_show")
+    closeBtnNav.style.transform = `rotate(0deg)`
+
+}
+
+const navToggle_hide = () => {
+    
+    nav.classList.remove("nav_show")
+    navBtn.classList.remove("display_hide")
+    bgLayer.classList.remove("bg_layer_nav_show")
+    
+}
+
+// Show 
+navBtn.addEventListener("click",navToggle_show)
+
+//Close nav 
+closeBtnNav.addEventListener('click',function(){
+    closeBtnNav.style.transform = `rotate(360deg)`
+    
+    setTimeout(()=>{
+        navToggle_hide()
+    },700)
+})
+bgLayer.addEventListener("click",function(){
+    closeBtnNav.style.transform = `rotate(360deg)`
+
+    setTimeout(()=>{
+        navToggle_hide()
+    },700)
+})
